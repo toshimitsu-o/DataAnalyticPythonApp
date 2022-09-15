c9a9ae9774816b8300dff0d0e7c0c835878b995d
Jupiner <59759734+Jupiner@users.noreply.github.com>
2022-09-15 23:57:29 +1000

    database code fixing

--- importing/createdatabase.py
+++ importing/createdatabase.py
@@ -1,33 +1,37 @@
 
 import sqlite3
 from sqlite3 import Error
+from importData import *
 
-
-def create_connection(db_file):
+def createDatabase(dbFile):
     """ create a database connection to a SQLite database """
     connection = None
     try:
-        connection = sqlite3.connect(db_file)
+        connection = sqlite3.connect(dbFile)
         c = connection.cursor()
         c.execute(
             """CREATE TABLE IF NOT EXISTS Accident
-            ([ACCIDENT_NO] STRING PRIMARY KEY,
-            [ACCIDENT_DATE], STRING,
-            [ACCIDENT_TIME],
-            [ACCIDENT_TYPE],
-            [DAY_OF_WEEK],
-            [SEVERITY],
-            [LONGITUDE],
-            [LATITUDE],
-            [LGA_NAME],
-            [REGION_NAME],
-            [FATALITY],
-            [SERIOUSINJURY],
-            [ALCOHOL_RELATED])"""
+            (ACCIDENT_NO VARCHAR(20) PRIMARY KEY,
+            ACCIDENT_DATE TEXT,
+            ACCIDENT_TIME VARCHAR,
+            ACCIDENT_TYPE VARHAR(30),
+            DAY_OF_WEEK VARCHAR(20),
+            SEVERITY VARCHAR(30),
+            LONGITUDE INTEGER,
+            LATITUDE INTEGER,
+            LGA_NAME VARCHAR(30),
+            REGION_NAME VARCHAR(30),
+            FATALITY ,
+            SERIOUSINJURY BOOLEAN,
+            ALCOHOL_RELATED BOOLEAN)"""
         )
     except Error as e:
         print(e)
-    finally:
-        if conn:
-            conn.close()
+        
+def insertData(dbFile):
+    """attempts to insert data into a database 
 
+    Args:
+        dbFile (_type_): _description_
+    """
+    connection = None
--- importing/importData.py
+++ importing/importData.py
@@ -5,61 +5,6 @@ from pycsvschema.checker import Validator
 import sys
 from csvvalidator import *
 
-def validateFile(fileName):
-    
-
-    
-    field_names = ("ACCIDENT_NO",
-                "ACCIDENT_DATE",
-                "ACCIDENT_TIME",
-                "ACCIDENT_TYPE",
-                "DAY_OF_WEEK",
-                "SEVERITY",
-                "LONGITUDE",
-                "LATITUDE",
-                "LGA_NAME",
-                "REGION_NAME",
-                "FATALITY",
-                "SERIOUSINJURY",
-                "ALCOHOL_RELATED"
-    )
-
-    # validator = CSVValidator(field_names)
-
-    # # basic header and record length checks
-    # validator.add_header_check('EX1', 'bad header')
-    # validator.add_record_length_check('EX2', 'unexpected record length')
-
-    # # # some simple value checks
-    # # validator.add_value_check('ACCIDENT_NO', str,
-    # #                         'EX3', 'study id must be an integer')
-    # # validator.add_value_check('patient_id', int,
-    # #                         'EX4', 'patient id must be an integer')
-    # # validator.add_value_check('gender', enumeration('M', 'F'),
-    # #                         'EX5', 'invalid gender')
-    # # validator.add_value_check('age_years', number_range_inclusive(0, 120, int),
-    # #                         'EX6', 'invalid age in years')
-    # # validator.add_value_check('date_inclusion', datetime_string('%Y-%m-%d'),
-    # #                         'EX7', 'invalid date')
-
-    # # # a more complicated record check
-    # # def check_age_variables(r):
-    # #     age_years = int(r['age_years'])
-    # #     age_months = int(r['age_months'])
-    # #     valid = (age_months >= age_years * 12 and
-    # #             age_months % age_years < 12)
-    # #     if not valid:
-    # #         raise RecordError('EX8', 'invalid age variables')
-    # # validator.add_record_check(check_age_variables)
-
-    # # # validate the data and write problems to stdout
-    # # data = csv.reader(fileName, delimiter='\t')
-    
-    # problems = validator.validate(fileName)
-    # write_problems(problems, sys.stdout)
-
-
-
 def importFile(fileName):
     
     """function accepts csv file, performs schema valiation and returns a list of tuples containing relevant
@@ -158,6 +103,59 @@ def importFile(fileName):
     
     
 importFile("C:/Users/zeefe/OneDrive/Documents/Uni/Year 2/Trimester 2/Software Technologies/Git Repositories/2810ICT-2022-Assignment/2810ICT-2022-Assignment/dataset/Crash Statistics Victoria.csv")
+
+    
+
+    
+#     field_names = ("ACCIDENT_NO",
+#                 "ACCIDENT_DATE",
+#                 "ACCIDENT_TIME",
+#                 "ACCIDENT_TYPE",
+#                 "DAY_OF_WEEK",
+#                 "SEVERITY",
+#                 "LONGITUDE",
+#                 "LATITUDE",
+#                 "LGA_NAME",
+#                 "REGION_NAME",
+#                 "FATALITY",
+#                 "SERIOUSINJURY",
+#                 "ALCOHOL_RELATED"
+#     )
+
+    # validator = CSVValidator(field_names)
+
+    # # basic header and record length checks
+    # validator.add_header_check('EX1', 'bad header')
+    # validator.add_record_length_check('EX2', 'unexpected record length')
+
+    # # # some simple value checks
+    # # validator.add_value_check('ACCIDENT_NO', str,
+    # #                         'EX3', 'study id must be an integer')
+    # # validator.add_value_check('patient_id', int,
+    # #                         'EX4', 'patient id must be an integer')
+    # # validator.add_value_check('gender', enumeration('M', 'F'),
+    # #                         'EX5', 'invalid gender')
+    # # validator.add_value_check('age_years', number_range_inclusive(0, 120, int),
+    # #                         'EX6', 'invalid age in years')
+    # # validator.add_value_check('date_inclusion', datetime_string('%Y-%m-%d'),
+    # #                         'EX7', 'invalid date')
+
+    # # # a more complicated record check
+    # # def check_age_variables(r):
+    # #     age_years = int(r['age_years'])
+    # #     age_months = int(r['age_months'])
+    # #     valid = (age_months >= age_years * 12 and
+    # #             age_months % age_years < 12)
+    # #     if not valid:
+    # #         raise RecordError('EX8', 'invalid age variables')
+    # # validator.add_record_check(check_age_variables)
+
+    # # # validate the data and write problems to stdout
+    # # data = csv.reader(fileName, delimiter='\t')
+    
+    # problems = validator.validate(fileName)
+    # write_problems(problems, sys.stdout)
+
 # importFile("C:/Users/zeefe/OneDrive/Documents/Uni/Year 2/Trimester 2/Software Technologies/Git Repositories/2810ICT-2022-Assignment/2810ICT-2022-Assignment/dataset/Crash Statistics Victoria.csv")
 
 
