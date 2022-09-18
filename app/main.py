20beb652985b13d27d5eb56d4d88d84796e0372d
Toshimitsu Ota <89127228+toshimitsu-o@users.noreply.github.com>
2022-09-18 17:27:28 +1000

    Grid widget added

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ Accident Analysis
 
 try:
     import wx
+    import wx.grid
 except ImportError:
     raise ImportError ("The wxPython module is required to run this program.")
 
@@ -45,6 +46,41 @@ class mainFrame(wx.Frame):
         box.SetBackgroundColour("white")
         # put some text with a larger bold font on it
         st = wx.StaticText(box, label="Main Box")
+
+        # Create a wxGrid object
+        grid = wx.grid.Grid(box, -1)
+
+        # Then we call CreateGrid to set the dimensions of the grid
+        # (100 rows and 10 columns in this example)
+        grid.CreateGrid(100, 10)
+
+        # We can set the sizes of individual rows and columns
+        # in pixels
+        grid.SetRowSize(0, 60)
+        grid.SetColSize(0, 120)
+
+        # And set grid cell contents as strings
+        grid.SetCellValue(0, 0, 'wxGrid is good')
+
+        # We can specify that some cells are read.only
+        grid.SetCellValue(0, 3, 'This is read.only')
+        grid.SetReadOnly(0, 3)
+
+        # Colours can be specified for grid cell contents
+        grid.SetCellValue(3, 3, 'green on grey')
+        grid.SetCellTextColour(3, 3, wx.GREEN)
+        grid.SetCellBackgroundColour(3, 3, wx.LIGHT_GREY)
+
+        # We can specify the some cells will store numeric
+        # values rather than strings. Here we set grid column 5
+        # to hold floating point values displayed with width of 6
+        # and precision of 2
+        grid.SetColFormatFloat(5, 6, 2)
+        grid.SetCellValue(0, 6, '3.1415')
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(grid, 1, wx.EXPAND, 5)
+        box.SetSizer(sizer)
 
         boxBtm = wx.StaticBox(pnl, wx.ID_ANY, "", pos =(0, 0), size =(780, 50))
         boxBtm.SetBackgroundColour("black")
