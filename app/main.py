bf9936918d784f133c4c9fb8e3d9a0ceca537c52
Toshimitsu Ota <89127228+toshimitsu-o@users.noreply.github.com>
2022-09-25 14:03:03 +1000

    import added

--- app/main.py
+++ app/main.py
@@ -22,83 +22,86 @@ class MainFrame(wx.Frame):
             
     def initialise(self):
 
+        # Define state of program: main, alcohol, location
+        self.menu = ""
+
         # create a panel in the frame
-        pnl = wx.Panel(self)
+        self.pnl = wx.Panel(self)
         self.frame_number = 1
-        self.SetBackgroundColour((19,162,166,255))
-        #self.SetBackgroundColour("white")
+        #self.SetBackgroundColour((19,162,166,255))
+        self.SetBackgroundColour("white")
 
         # Add Search box
-        searchBox = wx.StaticBox(pnl, wx.ID_ANY, "", pos =(0, 0), size =(-1, 80))
-        searchBox.SetBackgroundColour((247,247,247,255))
+        self.searchBox = wx.StaticBox(self.pnl, wx.ID_ANY, "", pos =(0, 0), size =(-1, 80))
+        self.searchBox.SetBackgroundColour((247,247,247,255))
         # Search box items
-        dateTl = wx.StaticText(searchBox, label="Date ")
-        dateFrCt = wx.TextCtrl(searchBox)
-        dateTo = wx.StaticText(searchBox, label="to ")
-        dateToCt = wx.TextCtrl(searchBox)
-        accTl = wx.StaticText(searchBox, label="Accident Type ")
-        accKyCt = wx.TextCtrl(searchBox)
-        typeList = ['Select from list', 'zero', 'one', 'two', 'three', 'four', 'five',
+        self.dateTl = wx.StaticText(self.searchBox, label="Date ")
+        self.dateFrCt = wx.TextCtrl(self.searchBox)
+        self.dateTo = wx.StaticText(self.searchBox, label="to ")
+        self.dateToCt = wx.TextCtrl(self.searchBox)
+        self.accTl = wx.StaticText(self.searchBox, label="Accident Type ")
+        self.accKyCt = wx.TextCtrl(self.searchBox)
+        self.typeList = ['Select from list', 'zero', 'one', 'two', 'three', 'four', 'five',
                       'six', 'seven', 'eight']
-        accCh = wx.Choice(searchBox, choices=typeList)
-        outTl = wx.StaticText(searchBox, label="Output ")
-        outCb1 = wx.CheckBox(searchBox, label = 'Day')
-        outCb2 = wx.CheckBox(searchBox, label = 'Hit&Run')
-        outCb3 = wx.CheckBox(searchBox, label = 'Location')
-        searchBtn = wx.Button(searchBox, label="Search", size=(100, 100))
+        self.accCh = wx.Choice(self.searchBox, choices=self.typeList)
+        self.outTl = wx.StaticText(self.searchBox, label="Output ")
+        self.outCb1 = wx.CheckBox(self.searchBox, label = 'Day')
+        self.outCb2 = wx.CheckBox(self.searchBox, label = 'Hit&Run')
+        self.outCb3 = wx.CheckBox(self.searchBox, label = 'Location')
+        self.searchBtn = wx.Button(self.searchBox, label="Search", size=(100, 100))
         # Sizer 
-        schSizer = wx.GridBagSizer(hgap=0, vgap=0)
+        self.schSizer = wx.GridBagSizer(hgap=0, vgap=0)
         # Date line
-        schSizer.Add(dateTl, pos=(0,0), flag=wx.ALIGN_LEFT)
-        vSizer = wx.BoxSizer(wx.HORIZONTAL)
-        vSizer.Add(dateFrCt, 0, wx.ALL, 0)
-        vSizer.Add(dateTo, 0, wx.ALL, 0)
-        vSizer.Add(dateToCt, 0, wx.ALL, 0)
-        schSizer.Add(vSizer, pos=(0,1), flag=wx.ALIGN_LEFT|wx.EXPAND)
+        self.schSizer.Add(self.dateTl, pos=(0,0), flag=wx.ALIGN_LEFT)
+        self.vSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.vSizer.Add(self.dateFrCt, 0, wx.ALL, 0)
+        self.vSizer.Add(self.dateTo, 0, wx.ALL, 0)
+        self.vSizer.Add(self.dateToCt, 0, wx.ALL, 0)
+        self.schSizer.Add(self.vSizer, pos=(0,1), flag=wx.ALIGN_LEFT|wx.EXPAND)
         # Accident type line
-        schSizer.Add(accTl, pos=(1,0), flag=wx.ALIGN_LEFT)
-        vSizer = wx.BoxSizer(wx.HORIZONTAL)
-        vSizer.Add(accKyCt, 0, wx.ALL, 0)
-        vSizer.Add(accCh, 0, wx.ALL, 0)
-        schSizer.Add(vSizer, pos=(1,1), flag=wx.ALIGN_LEFT|wx.EXPAND)
+        self.schSizer.Add(self.accTl, pos=(1,0), flag=wx.ALIGN_LEFT)
+        self.vSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.vSizer.Add(self.accKyCt, 0, wx.ALL, 0)
+        self.vSizer.Add(self.accCh, 0, wx.ALL, 0)
+        self.schSizer.Add(self.vSizer, pos=(1,1), flag=wx.ALIGN_LEFT|wx.EXPAND)
         # Output line
-        schSizer.Add(outTl, pos=(2,0), flag=wx.ALIGN_LEFT)
-        vSizer = wx.BoxSizer(wx.HORIZONTAL)
-        vSizer.Add(outCb1, 0, wx.ALL, 0)
-        vSizer.Add(outCb2, 0, wx.ALL, 0)
-        vSizer.Add(outCb3, 0, wx.ALL, 0)
-        schSizer.Add(vSizer, pos=(2,1), flag=wx.ALIGN_LEFT|wx.EXPAND)
-        schSizer.Add(searchBtn, pos=(0,2), span=(3,1), flag=wx.ALIGN_RIGHT|wx.ALIGN_BOTTOM|wx.EXPAND)
+        self.schSizer.Add(self.outTl, pos=(2,0), flag=wx.ALIGN_LEFT)
+        self.vSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.vSizer.Add(self.outCb1, 0, wx.ALL, 0)
+        self.vSizer.Add(self.outCb2, 0, wx.ALL, 0)
+        self.vSizer.Add(self.outCb3, 0, wx.ALL, 0)
+        self.schSizer.Add(self.vSizer, pos=(2,1), flag=wx.ALIGN_LEFT|wx.EXPAND)
+        self.schSizer.Add(self.searchBtn, pos=(0,2), span=(3,1), flag=wx.ALIGN_RIGHT|wx.ALIGN_BOTTOM|wx.EXPAND)
         # Set sizer
-        searchBox.SetSizer(schSizer)
+        self.searchBox.SetSizer(self.schSizer)
 
         # Box for summary
-        sumBox = wx.StaticBox(pnl, wx.ID_ANY, "", pos =(0, 0), size =(-1, -1))
-        sumBox.SetBackgroundColour("white")
+        self.sumBox = wx.StaticBox(self.pnl, wx.ID_ANY, "", pos =(0, 0), size =(-1, -1))
+        self.sumBox.SetBackgroundColour("white")
         # Items
-        sumTl = wx.StaticText(sumBox, label="Summary ")
-        found = wx.StaticText(sumBox, label="230 found (out of 23000)")
-        injury = wx.StaticText(sumBox, label="Injury: 200")
-        fatality = wx.StaticText(sumBox, label="fatality: 30")
+        self.sumTl = wx.StaticText(self.sumBox, label="Summary ")
+        self.found = wx.StaticText(self.sumBox, label="230 found (out of 23000)")
+        self.injury = wx.StaticText(self.sumBox, label="Injury: 200")
+        self.fatality = wx.StaticText(self.sumBox, label="fatality: 30")
         # Sizer
-        sumSizer = wx.BoxSizer(wx.VERTICAL)
-        sumSizer.Add(sumTl, 0, wx.ALL | wx.EXPAND, 0)
-        sumSizer.Add(found, 0, wx.ALL | wx.EXPAND, 0)
-        sumSizer.Add(injury, 0, wx.ALL | wx.EXPAND, 0)
-        sumSizer.Add(fatality, 0, wx.ALL | wx.EXPAND, 0)
-        sumBox.SetSizer(sumSizer)
+        self.sumSizer = wx.BoxSizer(wx.VERTICAL)
+        self.sumSizer.Add(self.sumTl, 0, wx.ALL | wx.EXPAND, 0)
+        self.sumSizer.Add(self.found, 0, wx.ALL | wx.EXPAND, 0)
+        self.sumSizer.Add(self.injury, 0, wx.ALL | wx.EXPAND, 0)
+        self.sumSizer.Add(self.fatality, 0, wx.ALL | wx.EXPAND, 0)
+        self.sumBox.SetSizer(self.sumSizer)
 
         # Search Bar
-        schbarSizer = wx.BoxSizer(wx.HORIZONTAL)
-        schbarSizer.Add(searchBox, 5, wx.ALL | wx.ALIGN_CENTER_VERTICAL | wx.EXPAND, 0)
-        schbarSizer.Add(sumBox, 1, wx.ALL | wx.ALIGN_TOP | wx.EXPAND, 0)
+        self.schbarSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.schbarSizer.Add(self.searchBox, 5, wx.ALL | wx.ALIGN_CENTER_VERTICAL | wx.EXPAND, 0)
+        self.schbarSizer.Add(self.sumBox, 1, wx.ALL | wx.ALIGN_TOP | wx.EXPAND, 0)
 
         # Box for grid
-        box = wx.StaticBox(pnl, wx.ID_ANY, "", pos =(0, 0), size =(-1, 450))
-        box.SetBackgroundColour("white")
+        self.box = wx.StaticBox(self.pnl, wx.ID_ANY, "", pos =(0, 0), size =(-1, 450))
+        self.box.SetBackgroundColour("white")
 
         # Create a wxGrid object
-        grid = wx.grid.Grid(box, -1)
+        grid = wx.grid.Grid(self.box, -1)
 
         # Then we call CreateGrid to set the dimensions of the grid
         # (100 rows and 10 columns in this example)
@@ -125,86 +128,141 @@ class MainFrame(wx.Frame):
         grid.SetCellValue(0, 6, '3.1415')
         # Set the whole grid read only
         grid.EnableEditing(False)
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(grid, 0, wx.ALL | wx.EXPAND, 0)
-        box.SetSizer(sizer)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(grid, 0, wx.ALL | wx.EXPAND, 0)
+        self.box.SetSizer(self.sizer)
         
         # Bottom box
-        boxBtm = wx.StaticBox(pnl, wx.ID_ANY, "", pos =(0, 0), size =(-1, 50))
-        boxBtm.SetBackgroundColour("black")
+        self.boxBtm = wx.StaticBox(self.pnl, wx.ID_ANY, "", pos =(0, 0), size =(-1, 50))
+        self.boxBtm.SetBackgroundColour("black")
         # Create items
-        charTl = wx.StaticText(boxBtm, label="Chart ")
-        charTl.SetForegroundColour("white")
-        cBtn1 = wx.Button(boxBtm, label="Hourly Average")
-        self.Bind(wx.EVT_BUTTON, self.onChartHour, cBtn1)
-        cBtn2 = wx.Button(boxBtm, label="Accident Types")
-        cBtn3 = wx.Button(boxBtm, label="By Month")
-        cBtn4 = wx.Button(boxBtm, label="By Day")
+        self.charTl = wx.StaticText(self.boxBtm, label="Chart ")
+        self.charTl.SetForegroundColour("white")
+        self.cBtn1 = wx.Button(self.boxBtm, label="Hourly Average")
+        self.Bind(wx.EVT_BUTTON, self.onChartHour, self.cBtn1)
+        self.cBtn2 = wx.Button(self.boxBtm, label="Accident Types")
+        self.cBtn3 = wx.Button(self.boxBtm, label="By Month")
+        self.cBtn4 = wx.Button(self.boxBtm, label="By Day")
         # Sizer
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
-        sizer.Add(charTl, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 0)
-        sizer.Add(cBtn1, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 0)
-        sizer.Add(cBtn2, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 0)
-        sizer.Add(cBtn3, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 0)
-        sizer.Add(cBtn4, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 0)
-        boxBtm.SetSizer(sizer)
+        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer.Add(self.charTl, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 0)
+        self.sizer.Add(self.cBtn1, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 0)
+        self.sizer.Add(self.cBtn2, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 0)
+        self.sizer.Add(self.cBtn3, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 0)
+        self.sizer.Add(self.cBtn4, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 0)
+        self.boxBtm.SetSizer(self.sizer)
         
         # and create a sizer to manage the layout of child widgets
-        menubox = wx.StaticBox(pnl, wx.ID_ANY, "", pos =wx.DefaultPosition, size =(-1, 50))
-        menubox.SetBackgroundColour((19,162,166,255))
+        self.menubox = wx.StaticBox(self.pnl, wx.ID_ANY, "", pos =wx.DefaultPosition, size =(-1, 50))
+        self.menubox.SetBackgroundColour((19,162,166,255))
         # button for Dataset
-        btn1 = wx.Button(pnl, size =(200, 40), label="Dataset")
-        bmp = wx.Bitmap('btn.png', wx.BITMAP_TYPE_PNG).ConvertToImage()
-        bmp = wx.Bitmap(bmp.Scale(30, 30, wx.IMAGE_QUALITY_HIGH))
-        btn1.SetBitmap(bmp)
-        self.Bind(wx.EVT_BUTTON, self.onDataset, btn1)
+        self.btn1 = wx.Button(self.pnl, size =(200, 40), label="Dataset")
+        self.bmp = wx.Bitmap('btn.png', wx.BITMAP_TYPE_PNG).ConvertToImage()
+        self.bmp = wx.Bitmap(self.bmp.Scale(30, 30, wx.IMAGE_QUALITY_HIGH))
+        self.btn1.SetBitmap(self.bmp)
+        self.Bind(wx.EVT_BUTTON, self.onDataset, self.btn1)
         #  button for Analyse
-        btn2 = wx.Button(pnl, size =(200, 40), label="Analyse")
-        bmp = wx.Bitmap('btn.png', wx.BITMAP_TYPE_PNG).ConvertToImage()
-        bmp = wx.Bitmap(bmp.Scale(30, 30, wx.IMAGE_QUALITY_HIGH))
-        btn2.SetBitmap(bmp)
-        self.Bind(wx.EVT_BUTTON, self.onAnalyse, btn2)
+        self.btn2 = wx.Button(self.pnl, size =(200, 40), label="Analyse")
+        self.bmp = wx.Bitmap('btn.png', wx.BITMAP_TYPE_PNG).ConvertToImage()
+        self.bmp = wx.Bitmap(self.bmp.Scale(30, 30, wx.IMAGE_QUALITY_HIGH))
+        self.btn2.SetBitmap(self.bmp)
+        self.Bind(wx.EVT_BUTTON, self.onAnalyse, self.btn2)
         #  button for Analyse
-        btn3 = wx.Button(pnl, size =(200, 40), label="Alcohol")
-        bmp = wx.Bitmap('btn.png', wx.BITMAP_TYPE_PNG).ConvertToImage()
-        bmp = wx.Bitmap(bmp.Scale(30, 30, wx.IMAGE_QUALITY_HIGH))
-        btn3.SetBitmap(bmp)
-        self.Bind(wx.EVT_BUTTON, self.onAlcohol, btn3)
+        self.btn3 = wx.Button(self.pnl, size =(200, 40), label="Alcohol")
+        self.bmp = wx.Bitmap('btn.png', wx.BITMAP_TYPE_PNG).ConvertToImage()
+        self.bmp = wx.Bitmap(self.bmp.Scale(30, 30, wx.IMAGE_QUALITY_HIGH))
+        self.btn3.SetBitmap(self.bmp)
+        self.Bind(wx.EVT_BUTTON, self.onAlcohol, self.btn3)
         #  button for Location
-        btn4 = wx.Button(pnl, size =(200, 40), label="Location")
-        bmp = wx.Bitmap('btn.png', wx.BITMAP_TYPE_PNG).ConvertToImage()
-        bmp = wx.Bitmap(bmp.Scale(30, 30, wx.IMAGE_QUALITY_HIGH))
-        btn4.SetBitmap(bmp)
-        self.Bind(wx.EVT_BUTTON, self.onLocation, btn4)
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
-        sizer.Add(btn1, 0, wx.EXPAND, 0)
-        sizer.Add(btn2, 0, wx.EXPAND, 0)
-        sizer.Add(btn3, 0, wx.EXPAND, 0)
-        sizer.Add(btn4, 0, wx.EXPAND, 0)
-        menubox.SetSizer(sizer)
+        self.btn4 = wx.Button(self.pnl, size =(200, 40), label="Location")
+        self.bmp = wx.Bitmap('btn.png', wx.BITMAP_TYPE_PNG).ConvertToImage()
+        self.bmp = wx.Bitmap(self.bmp.Scale(30, 30, wx.IMAGE_QUALITY_HIGH))
+        self.btn4.SetBitmap(self.bmp)
+        self.Bind(wx.EVT_BUTTON, self.onLocation, self.btn4)
+        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer.Add(self.btn1, 0, wx.EXPAND, 0)
+        self.sizer.Add(self.btn2, 0, wx.EXPAND, 0)
+        self.sizer.Add(self.btn3, 0, wx.EXPAND, 0)
+        self.sizer.Add(self.btn4, 0, wx.EXPAND, 0)
+        self.menubox.SetSizer(self.sizer)
         
-
-        # Sizer for main structure
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(menubox, 0, wx.ALL | wx.EXPAND,0)
-        sizer.Add(schbarSizer, 0, wx.ALL | wx.EXPAND, 0)
-        sizer.Add(box, 5, wx.ALL |wx.EXPAND, 0)
-        sizer.Add(boxBtm, 0, wx.ALL |wx.EXPAND | wx.ALIGN_BOTTOM, 0)
-        pnl.SetSizer(sizer)
+        # Dataset import
+        self.importBox = wx.StaticBox(self.pnl, wx.ID_ANY, "", pos =wx.DefaultPosition, size =(-1, 100))
+        self.importBox.SetBackgroundColour((247,247,247,255))
+        # Items
+        self.importTl = wx.StaticText(self.importBox, label="Import Dataset ")
+        #importTl.SetForegroundColour("white")
+        self.importBtn = wx.Button(self.importBox, label="Select a file")
+        self.Bind(wx.EVT_BUTTON, self.onFileOpen, self.importBtn)
+        # Sizer
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.importTl, 0, wx.ALL | wx.EXPAND,0)
+        self.sizer.Add(self.importBtn, 0, wx.ALL | wx.EXPAND,0)
+        self.importBox.SetSizer(self.sizer)
 
         # create a menu bar
         self.makeMenuBar()
 
+        # Build main content structure
+        if self.menu == "main":
+            self.buidImport()
+        else:
+            self.buidMain()
+
         # and a status bar
         self.CreateStatusBar()
         self.SetStatusText("Welcome to "+ APP_NAME)
+    
+    def buidImport(self):
+        # Sizer for main structure
+        self.importSizer = wx.BoxSizer(wx.VERTICAL)
+        self.importSizer.Add(self.menubox, 0, wx.ALL | wx.EXPAND,0)
+        self.importSizer.Add(self.importBox, 5, wx.ALL |wx.EXPAND | wx.ALIGN_CENTER_VERTICAL, 0)
+        self.importSizer.Add(self.boxBtm, 0, wx.ALL |wx.EXPAND | wx.ALIGN_BOTTOM, 0)
+        self.pnl.SetSizer(self.importSizer)
 
+    def buidMain(self):
+        # Sizer for main structure
+        self.mainSizer = wx.BoxSizer(wx.VERTICAL)
+        self.mainSizer.Add(self.menubox, 0, wx.ALL | wx.EXPAND,0)
+        self.mainSizer.Add(self.schbarSizer, 0, wx.ALL | wx.EXPAND, 0)
+        self.mainSizer.Add(self.box, 5, wx.ALL |wx.EXPAND, 0)
+        self.mainSizer.Add(self.boxBtm, 0, wx.ALL |wx.EXPAND | wx.ALIGN_BOTTOM, 0)
+        self.pnl.SetSizer(self.mainSizer)
+
+    def onFileOpen(self, event):
+        # Ask the user what new file to open
+        with wx.FileDialog(self, "Open XYZ file", wildcard="CSV files (*.csv)|*.csv",
+                       style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
+
+            if fileDialog.ShowModal() == wx.ID_CANCEL:
+                return     # the user changed their mind
+
+            # Proceed loading the file chosen by the user
+            pathname = fileDialog.GetPath()
+            try:
+                with open(pathname, 'r') as file:
+                    self.loadFile(file)
+            except IOError:
+                wx.LogError("Cannot open file '%s'." % newfile)
+
+    def loadFile(self, filepath):
+        filepath = str(filepath)
+        self.menu = "main"
+        #self.pnl.Update()
+        wx.MessageBox("Filepath for Dataset is: " + filepath)
 
     def onDataset(self, event):
         wx.MessageBox("Dataset will happen")
 
     def onAnalyse(self, event):
-        wx.MessageBox("Analyse will happen")
+        self.menu = "main"
+        self.importSizer.Destroy()
+        self.importBox.Destroy()
+        #self.buidMain()
+        self.Update()
+
+        #wx.MessageBox("Analyse will happen")
 
     def onAlcohol(self, event):
         wx.MessageBox("Alcohol will happen")
